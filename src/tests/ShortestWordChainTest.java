98f638bbe527ec72fe3d9cd5bab13d7dfec9520d
oking <ollyking123@gmail.com>
2014-11-12 10:30:04 +0000

    Added ShortestWordChain test

--- src/tests/ShortestWordChainTest.java
+++ src/tests/ShortestWordChainTest.java
@@ -2,9 +2,14 @@ package tests;
 
 import main.dictionary.Dictionary;
 import main.input.WordChainInput;
+import main.output.Path;
 import main.wordchainsolver.AllWordChains;
 import main.wordchainsolver.ShortestWordChain;
 import org.junit.Test;
+
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertTrue;
 
 /**
  * Created by oking on 12/11/14.
@@ -15,8 +20,21 @@ public class ShortestWordChainTest {
     private Dictionary dictionary;
     private ShortestWordChain shortestWordChain;
 
+    //Need to add parameter testing to test more cases.
     @Test
-    public void testIsShortestChain(){
+    public void testIsShortestChain() throws IOException {
 
+        input = new WordChainInput("lead", "gold");
+        dictionary = new Dictionary(4);
+        shortestWordChain = new ShortestWordChain(input, dictionary);
+        Path expectedPath = new Path();
+        expectedPath.addWord("gold");
+        expectedPath.addWord("goad");
+        expectedPath.addWord("load");
+        expectedPath.addWord("lead");
+
+        Path resultPath = shortestWordChain.findShortestChain();
+
+        assertTrue(expectedPath.equals(resultPath));
     }
 }
